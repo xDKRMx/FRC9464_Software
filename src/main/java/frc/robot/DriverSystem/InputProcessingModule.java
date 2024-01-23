32d091e3de70acf2e6a298d65b357e9ad2f824c3
kerem inci <kereminci2007@gmail.com>
2024-01-23 17:01:46 +0300

    HIZ KONTROL İŞLEMLERİ

--- src/main/java/frc/robot/DriverSystem/InputProcessingModule.java
+++ src/main/java/frc/robot/DriverSystem/InputProcessingModule.java
@@ -1,5 +1,7 @@
 package frc.robot.DriverSystem;
 
+import javax.swing.text.html.parser.Element;
+
 import edu.wpi.first.wpilibj.Joystick;
 
 //Simülasyonu bilgisayarda yapabilmek için klavye analog ataması
@@ -13,16 +15,20 @@ public  class InputProcessingModule {
      //Joystick tanımlamaları
      Joystick Left_Joystick = new Joystick(0);
      Joystick Right_Joystick = new Joystick(1); 
+     private int Active_left_button = -1;
+     private int Active_right_button = -1;
+     private 
      // Klavye Analog Tanımlaması 
      KeyboardAnalog Add_Keyboard = new KeyboardAnalog();
      //Composition ve Encapsulation mantığı ile modüllerin Class içerisine çağırılıp örneklerinin alınması
      private MotorControllerModule Motor_Controller_Module;
+     /***************************/ 
       // Constructor
      public InputProcessingModule(MotorControllerModule M_C_Module)
      {
         Motor_Controller_Module = M_C_Module;
      }
-
+      /***************************/
       /*|region : JOYSTICK GİRİŞ İŞLEMLERİ |*/
       
       //Periodic Metot // Hem teleop hem de Autonomous için kullanılabilir
@@ -32,8 +38,9 @@ public  class InputProcessingModule {
         if(Is_It_Teleop) 
         {
          //Driver station üzerinden joysticklerin bağlı olup olmadığını kontrol ediyoruz eğer ki bağlı değilse keyboard üzerinden işlem yapmasını istiyoruz
-         if(Left_Joystick.isConnected() && Right_Joystick.isConnected()) Motor_Controller_Module.Teleop_Drive_Periodic(Right_Joystick, Left_Joystick);
+         if(Connection_Check(Left_Joystick,Right_Joystick)) Motor_Controller_Module.Teleop_Drive_Periodic(Right_Joystick, Left_Joystick);
          else Motor_Controller_Module.Teleop_Drive_Periodic(null,null);
+
         }
         else
         {
@@ -43,4 +50,39 @@ public  class InputProcessingModule {
        }
 
       /*|Endregion : JOYSTICK GİRİŞ İŞLEMLERİ |*/
+      /***************************/
+      /*| Region : JOYSTICK DÜĞME İŞLEME| */
+      public int[] Joystick_Button_Processing()
+      {
+          //Left JoystickControl
+          if(Left_Joystick.getRawButton(1))   Active_left_button = 1;
+          else if(Left_Joystick.getRawButton(2)) Active_left_button = 2;
+          else if(Left_Joystick.getRawButton(3)) Active_left_button = 3;
+          else if(Left_Joystick.getRawButton(4)) Active_left_button = 4;
+          else Active_left_button = -1;
+          if(Right_Joystick.getRawButton(1))   Active_right_button = 1;
+          else if(Right_Joystick.getRawButton(2)) Active_right_button = 2;
+          else if(Right_Joystick.getRawButton(3)) Active_right_button = 3;
+          else if(Right_Joystick.getRawButton(4)) Active_right_button = 4;
+          else Active_right_button = -1;
+          int[] Button_value_list = {Active_left_button, Active_right_button};
+          return Button_value_list;
+      }
+
+       /*| End Region : JOYSTICK DÜĞME İŞLEME| */
+      /***************************/
+      /*| Region : HATA YÖNETİMİ VE GÜVENLİK KONTROL| */
+       //Joysticklerden gelen sinyalleri işlemek için algoritma
+       //Periodic metot
+     
+
+      //Joysticklerin ve klavyenin bağlantısını kontrol etmek için algoritma
+      //Kontrol metodu
+      public boolean Connection_Check(Joystick Left, Joystick Right)
+      {
+        // Joysticklerin  bağlantısını kontrol etmek için algoritmanın fonksiyonunu burada çağırın
+          boolean Is_Connected = Left.isConnected() && Right.isConnected();
+          return Is_Connected;
+      }
+      /*| Region : JOYSTICK GİRİŞ İŞLEMLERİ| */
 }
--- src/main/java/frc/robot/DriverSystem/MotorControllerModule.java
+++ src/main/java/frc/robot/DriverSystem/MotorControllerModule.java
@@ -9,11 +9,18 @@ import java.util.Collections;
 
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 //Simülasyonu çalıştırmak için Keyboard Analog importu (Geçici)
 import frc.robot.DriverSystem.AdditionalClasses.*;
 
+//BU ENUM yapısı robotun motor durum kontrolünde kullanılacaktır ve ilerleyen aşamalarda sensör entegrasyonundaki veirlerle birlikte sağlanan değerler enum'daki robotun durumna göre şekillenecektir
+ enum RobotStatus {
+  DYNAMIC,
+  IDLE,
+  TURNING
+}
 public  class MotorControllerModule {
      //Bu class bir nevi bizim için bir çok class içerisinde kullanacağımız ve robottaki bir çok kodun temelini oluşturacak bir class. 
      // Bu class içerisindeki işlemler bir nevi robotun içerisindeki temel dinamikler diyebilirim bu temel dinamiklere örnek olarak sürüş, dönme hareket durma ivmeli hareket vs.
@@ -21,6 +28,9 @@ public  class MotorControllerModule {
     /************************************************** */ 
     //Ek modüllerin tanımlaması
     KeyboardAnalog Key_Analog = new KeyboardAnalog();
+    SensorIntegrationModule Sensor_Integration = new SensorIntegrationModule();
+    //Robotun durumu
+    public RobotStatus robot_Status;
     // Motorların tanımlamaları
     private final  CANSparkMax Left_Leader = new CANSparkMax(0,MotorType.kBrushless);
     private final  CANSparkMax Left_Follower= new CANSparkMax(1,MotorType.kBrushless);
@@ -30,14 +40,22 @@ public  class MotorControllerModule {
      public DifferentialDrive Main_Robot_Drive = new DifferentialDrive(Left_Leader, Right_Leader);
      //Motora ait bazı verilerin Tanımlamaları
      private ArrayList<Double> Speed_Of_Each_Motors;
+     
+     // PID değişkenlerinin tanımlamaları
+     private double previous_error = 0.0;
+     private double integral_sum = 0.0;
+     private double dt = 0.01;
+     private ArrayList<Double> PID_Coefficients;
+     /**********************************/
      // Constructor
      public MotorControllerModule()
      {
         Speed_Of_Each_Motors = new ArrayList<>(Collections.nCopies(2, 0.0));
         Left_Leader.set(0);
         Right_Leader.set(0);
+        
      }
-     
+     /***************************/
      /*|region : PWM MOTOR KONTROL  |*/
 
      // Periodic Metot // Hem Teleoperation hem de Autonomous için kullanılabilir 
@@ -54,8 +72,18 @@ public  class MotorControllerModule {
            //Burada bu joysticklerdeki axis'lerin itilme miktarını çektik
            if(leftJoystick != null && rightJoystick != null)
            {
-            Speed_Of_Each_Motors.set(0, leftJoystick.getY());
-            Speed_Of_Each_Motors.set(1, rightJoystick.getY());
+            // Mevcut motor hızlarını al
+             double currentLeftSpeed = Speed_Of_Each_Motors.get(0);
+             double currentRightSpeed = Speed_Of_Each_Motors.get(1);
+
+             // Joystick inputlarını al veya varsayılan olarak 0 kabul et
+             double leftJoystickInput = leftJoystick.getY();
+             double rightJoystickInput =  rightJoystick.getY();
+             // Ramp algoritmasına göre Stabiliteyi sağlayacak bir ayarlama işlemi
+             Double Absolute_Left_Motor_Speed = rampMotorInput(currentLeftSpeed,leftJoystickInput,0.1f);
+             Double Absolute_Right_Motor_Speed = rampMotorInput(currentRightSpeed,rightJoystickInput,0.1f);
+            Speed_Of_Each_Motors.set(0, Absolute_Left_Motor_Speed);
+            Speed_Of_Each_Motors.set(1,Absolute_Right_Motor_Speed );
            }
            else
            {
@@ -82,8 +110,70 @@ public  class MotorControllerModule {
           //* */
           //Robotumuzu tank Drive bir şekilde geliştireceğimiz için bu tanımladığımız Right ve Left Leader'laarın motorlarına verilen bir güç olmalı 
          //Bu gücü de Input Processing Class'i içerisinde tanımlanmış joystick'lerin yön tuşlarındaki kolun ne kadar itildiğine bağlı olarak alacağı değere bağlı olacak. Artı olarak parametrelere değerini Input Processing'deki Joystickler Verecek 
+         Motor_Stability(0.5d);
           Main_Robot_Drive.tankDrive( Speed_Of_Each_Motors.get(0),Speed_Of_Each_Motors.get(1));
       }
+       //Motorlara verilen güçlerin stabilitesi ve max motor gücü farkı
+       void Motor_Stability(Double max_Difference)
+       {
+         double Current_Difference = Math.abs(Speed_Of_Each_Motors.get(0) - Speed_Of_Each_Motors.get(1));
+        // Current Difference Control
+        //İLK ALTERNATİF
+         while(Current_Difference > max_Difference)
+         {
+            PIDController PID_ = new PIDController(0.1,0.01,0.5);
+            Double output =PID_.calculate(Current_Difference, Current_Difference);
+
+            Current_Difference /= 2;
+            if(Current_Difference <= max_Difference)
+           {
+            Double Half_difference = Current_Difference /2;
+            Boolean extensial = Speed_Of_Each_Motors.get(0) > Speed_Of_Each_Motors.get(1);
+            if(extensial)
+            {
+               Speed_Of_Each_Motors.set(0,Speed_Of_Each_Motors.get(0)-Half_difference) ;
+               Speed_Of_Each_Motors.set(1,Speed_Of_Each_Motors.get(1)+Half_difference) ;
+
+            }
+            else
+            {
+                Speed_Of_Each_Motors.set(0,Speed_Of_Each_Motors.get(0)+Half_difference) ;
+               Speed_Of_Each_Motors.set(1,Speed_Of_Each_Motors.get(1)-Half_difference) ;
+            }
+           }
+         }
+          //İKİNCİ ALTERNATİF
+         // if(Current_Difference > max_Difference)
+         // {
+         //    if(Speed_Of_Each_Motors.get(0) > Speed_Of_Each_Motors.get(1))
+         //    {
+         //       double Half_difference = Current_Difference / 2;
+         //         Speed_Of_Each_Motors.set(1,Speed_Of_Each_Motors.get(1)+Half_difference);
+         //    }
+         //    else
+         //    {
+         //        double Half_difference = Current_Difference / 2;
+         //         Speed_Of_Each_Motors.set(0,Speed_Of_Each_Motors.get(0)+Half_difference);
+         //    }
+         // }
+       }
+
+       //Ramping algoritması ile Klavye analogundaki ani hızlanmaları engellediğimiz gibi Joystick'de ani hızlanmaları engelleyip daha pürüzsüz ve akıcı bir hızlanma sağlıyor 
+       private double rampMotorInput(double currentSpeed, double joystickInput, double RAMP_RATE) {
+         if (Math.abs(joystickInput - currentSpeed) > RAMP_RATE) {
+             // Joystick input, mevcut hızdan yeterince farklıysa ramp yap
+             if (currentSpeed < joystickInput) {
+                 currentSpeed += RAMP_RATE;
+             } else if (currentSpeed > joystickInput) {
+                 currentSpeed -= RAMP_RATE;
+             }
+         } else {
+             // Joystick input ile mevcut hız arasındaki fark çok küçükse, inputu direkt kullan
+             currentSpeed = joystickInput;
+         }
+         // Hızı -1 ile 1 arasında sınırla
+         return Math.max(-1.0, Math.min(1.0, currentSpeed));
+     }
 
       //Sensör entegrasyon ve Telemetri modüllerinde bu tarz fonksiyonlardan yararlanarak robotun verilerini alacağız
       public ArrayList<Double> Get_Speed_Of_Each_Motors()
@@ -97,7 +187,69 @@ public  class MotorControllerModule {
           Can_Spark.add(Right_Leader);
          return Can_Spark;
       }
-       /*|Endregion : PWM MOTOR KONTROL  |*/
-}
 
+       /*|Endregion : PWM MOTOR KONTROL  |*/
+      
+        /***************************/
+
+       /* | Region : MOTOR DURUM İZLEME  |*/
+
+
+       // !!! PID SİSTEMİ İLE KONTROL YAPILMAKTADIR !!! 
+       //Periodic Metot // Hem Teleop Hem de Autonomous için kullanılabilir
+        public void Motor_Velocity_Equation(Double Setpoint, Double Current_Speed, Double Current_Direction)
+        {
+            // Robotun durumunu kontrol et
+           if(robot_Status != RobotStatus.IDLE)
+           {
+            // PID kontrol algoritmasını çalıştır
+           //PID'nin coefficient değerlerini PID_parametres fonksiyonundaki değerler belirliyor
+           PIDController pid_controller = new PIDController(PID_Coefficients.get(0),PID_Coefficients.get(1), PID_Coefficients.get(2));
+           double output = pid_controller.calculate(Setpoint, Current_Speed);
+
+           // Motorların hız kontrol sinyallerini hesapla
+           double left_motor_speed = output;
+           double right_motor_speed = output;
+
+           // Motorların hız kontrol sinyallerini uygula
+           Speed_Of_Each_Motors.set(0, left_motor_speed);
+           Speed_Of_Each_Motors.set(1, right_motor_speed);
+
+           // Önceki değerleri kaydet
+           double error = Setpoint - Current_Speed;
+           previous_error = error;
+           integral_sum += error *dt;
+           }
+          
+        }
+      public ArrayList<Double> PID_parametres(double k_Proportional, double k_Integral, double k_derivative)
+      {
+        PID_Coefficients.set(0, k_Proportional) ;
+        PID_Coefficients.set(1, k_Integral) ;
+        PID_Coefficients.set(2, k_derivative) ;
+        return PID_Coefficients;
+      }
+      public void Robot_Status_Control()
+      {
+         // ROBOTUN  MOTORLARINDAKİ HIZINA GÖRE MOTORUN BULUNDUĞU HALİ KONTROL EDİYORUZ
+         if(Speed_Of_Each_Motors.get(0) < 0.025d && Speed_Of_Each_Motors.get(1) < 0.025d )
+         {
+            Speed_Of_Each_Motors.set(0, 0d);
+            Speed_Of_Each_Motors.set(1, 0d);
+           robot_Status = RobotStatus.IDLE;
+         }
+         else 
+         {
+             robot_Status = RobotStatus.DYNAMIC;
+         }
+
+         //ayrı olarak Robot dinamikse de bir yere gidiyor mu yoksa dönüyor mu bunun kontrolü
+         if(Math.abs(Speed_Of_Each_Motors.get(0) - Speed_Of_Each_Motors.get(1)) > 0.3)
+         {
+          robot_Status = RobotStatus.TURNING;
+         }
+      }
+       /*|Endregion :  MOTOR DURUM İZLEME |*/
+ }
+      
 
